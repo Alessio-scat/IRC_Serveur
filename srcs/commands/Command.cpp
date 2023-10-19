0b56aa1d92944094b3896fbae7cf1ff8a4e2984f
Lucas-Ve <lveloso@student.42lyon.fr>
2023-10-19 15:28:54 +0200

    command join add

--- srcs/commands/Command.cpp
+++ srcs/commands/Command.cpp
@@ -21,5 +21,35 @@ Command Command::operator=(Command const &rhs)
 
 void Command::whatCommand(char *buffer)
 {
-    std::cout << buffer << std::endl;
+    std::string str;
+    std::istringstream iss(buffer);
+    size_t pos;
+    while (std::getline(iss, str))
+    {
+        pos = str.find("KICK");
+        if (pos !=  std::string::npos)
+        {
+            std::cout << "KICK command : " << str << std::endl;
+        }
+        pos = str.find("INVITE");
+        if (pos !=  std::string::npos)
+        {
+            std::cout << "INVITE command : " << str << std::endl;
+        }
+        pos = str.find("TOPIC");
+        if (pos !=  std::string::npos)
+        {
+            std::cout << "TOPIC command : " << str << std::endl;
+        }
+        pos = str.find("MODE");
+        if (pos !=  std::string::npos)
+        {
+            std::cout << "MODE command : " << str << std::endl;
+        }
+        pos = str.find("JOIN");
+        if (pos !=  std::string::npos)
+        {
+            std::cout << "JOIN command : " << str << std::endl;
+        }
+    }
 }
