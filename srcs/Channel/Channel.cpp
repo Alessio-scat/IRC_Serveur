0b8efeb79164df0b19fdc7428c950e942d87ef6b
Axel Vaganay <avaganay@z2r1p6.42lyon.fr>
2023-11-06 13:11:36 +0100

    invite _isActive

--- srcs/Channel/Channel.cpp
+++ srcs/Channel/Channel.cpp
@@ -14,3 +14,22 @@ Channel &Channel::operator=(Channel const &rhs)
 }
 
 Channel::~Channel(void){}
+
+int isInChannel(std::string channelFind, std::string nick, Channel &channel)
+{
+    std::map<std::string, std::list<std::string> >::iterator it = channel.channel.begin();
+    for (; it != channel.channel.end(); ++it)
+    {
+        if (it->first == channelFind)
+            break ;
+    }
+    if (it != channel.channel.end())
+    {
+        for (std::list<std::string>::iterator it2 = it->second.begin(); it2 != it->second.end(); ++it2)
+        {
+            if (*it2 == nick)
+                return (0);
+        }
+    }
+    return (1);
+}
