bea8fbf427e851539a0f180be86cd324f0ac048a
Lucas-Ve <lveloso@student.42lyon.fr>
2023-11-06 11:31:47 +0100

    comment

--- srcs/commands/Message.cpp
+++ srcs/commands/Message.cpp
@@ -1,18 +1,18 @@
 #include "../../includes/commands/Command.hpp"
 
-Message::Message(void){}
+// Message::Message(void){}
 
-Message::Message(Message const &src)
-{
-    *this = src;
-}
+// Message::Message(Message const &src)
+// {
+//     *this = src;
+// }
 
-Message::Message(std::string str, User &user)
-{
+// Message::Message(std::string str, User &user)
+// {
 
-}
+// }
 
-Message::~Message(){}
+// Message::~Message(){}
 
 // Message Message::operator=(Message const &rhs)
 // {
