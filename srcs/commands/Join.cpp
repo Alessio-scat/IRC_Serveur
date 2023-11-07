abcebac550e1f70ff29ceb097e8e0614111c96a8
ascatama@student.42lyon.fr <ascatama@student.42lyon.fr>
2023-11-07 15:52:49 +0100

    sort a token in ParseJoinCmd

--- srcs/commands/Join.cpp
+++ srcs/commands/Join.cpp
@@ -15,49 +15,59 @@ void Join::execute_cmd(std::string str)
     A faire:
         -  ERR_NEEDMOREPARAMS (461) 
         -  ERR_BADCHANNELKEY (475) 
-        -  ERR_INVITEONLYCHAN (473)
+        -  OK: ERR_INVITEONLYCHAN (473)
         -  RPL_NAMREPLY (353) 
         -  RPL_ENDOFNAMES (366) 
 */
 
 void Join::ParseJoinCmd(std::string &str)
 {
-    std::istringstream ss(str);
-    std::string token;
-    int i = 0;
-    // ft_trim(str);
+    // std::istringstream ss(str);
+    // std::string token;
+    // int i = 0;
 
-    while (ss >> token)
-    {
-        std::cout << UNDER << "str|" << token << "|" << RESET << std::endl;
-        if (i == 0)
-        {
-            _cmd = token;
-            std::cout << UNDER << "cmd|" << _cmd << "|" << RESET << std::endl;
-            i = 1;
+    std::istringstream iss(str);
+    std::string token;
+    std::vector<std::string> tokens;
+
+    while (std::getline(iss, token, ' ')) {
+        // Divise la chaîne en utilisant l'espace comme délimiteur
+        std::istringstream iss2(token);
+        std::string subtoken;
+
+        while (std::getline(iss2, subtoken, ',')) {
+            // Divise la sous-chaîne en utilisant la virgule comme délimiteur
+            ft_trim(subtoken);
+            tokens.push_back(subtoken);
         }
-        else if (i == 1)
-        {
-            _channelJoin = token;
-            std::cout << UNDER << "_channelJoin|" << _channelJoin << "|" << RESET << std::endl;
-            i = 2;
-        }
-        // else if (i == 2)
-        // {
-        //     if (token == "IRChub")
-        //     {
-        //         _channelInvite = "";
-        //         break;
-        //     }
-        //     _channelInvite = token;
-        //     std::cout << UNDER << "_channelInvite|" << _channelInvite << "|" << RESET << std::endl;
-        //     break;
-        // }
     }
+
+   for (std::vector<std::string>::const_iterator it = tokens.begin(); it != tokens.end(); ++it) {
+    const std::string& t = *it;
+    std::cout << "|" << t << "|" << std::endl;
+    }
+
+    // while (ss >> token)
+    // {
+    //     std::cout << UNDER << "str|" << token << "|" << RESET << std::endl;
+    //     if (i == 0)
+    //     {
+    //         _cmd = token;
+    //         std::cout << UNDER << "cmd|" << _cmd << "|" << RESET << std::endl;
+    //         i = 1;
+    //     }
+    //     else if (i == 1)
+    //     {
+    //         _channelJoin = token;
+    //         std::cout << UNDER << "_channelJoin|" << _channelJoin << "|" << RESET << std::endl;
+    //         i = 2;
+    //     }
+    // }
 }
 
 void Join::add_user_inChannel(Channel &channel, User *_tabUser, Join &join, int i, std::deque<struct pollfd> _pfds)
 {
+    std::cout << BOLD << "ICI" << RESET << std::endl;
     std::string list;
     std::string word;
 
@@ -70,7 +80,7 @@ void Join::add_user_inChannel(Channel &channel, User *_tabUser, Join &join, int 
         for (int j = 1;j <= MAXCLIENT; j++)
         {
             if (word == _tabUser[j].getNickname() || word == "@" + _tabUser[j].getNickname())
-                writeInfd(RPL_NAMREPLY(_tabUser[i].getNickname(), join.nameChannel, list), i, _pfds);
+                writeInfd(RPL_NAMREPLY(_tabUser[i].getNickname(), join.nameChannel, list), j, _pfds);
         }
     }
 }
@@ -80,8 +90,8 @@ int Join::verifModeChannel(Channel &channel, User *_tabUser, int y)
 {
     int isMode = 0;
     int isInvitation = 0;
-    for (std::map<std::string, std::vector<char> >::iterator it = channel.mapMode.begin(); it != channel.mapMode.end(); ++it) {
-        // std::cout << GREEN << "Clé : " << it->first << " - Valeur : " << RESET ;
+    for (std::map<std::string, std::vector<char> >::iterator it = channel.mapMode.begin(); it != channel.mapMode.end(); ++it)
+    {
         if (_channelJoin == it->first)
         {
             for (std::vector<char>::iterator vecIt = it->second.begin(); vecIt != it->second.end(); ++vecIt)
@@ -91,19 +101,10 @@ int Join::verifModeChannel(Channel &channel, User *_tabUser, int y)
                     isMode = 1;
                     std::cout << "IL Y UN MODE !!" << std::endl;
                 }
-                // char c = *vecIt;
-                // std::cout << GREEN << c << ' ' << RESET;
             }
         }
-        // std::cout << std::endl;
     }
 
-    // int y;
-    // for (y = 0; y < MAX_USERS; y++)
-    // {
-    //     if (_tabUser[y].getNickname() == _nickInvite)
-    //         break ;
-    // }
     std::cout << GREEN << _tabUser[y].getNickname() << RESET << std::endl;
     for (std::map<bool, std::list<std::string> >::iterator it = _tabUser[y]._mapModeUser.begin(); it != _tabUser[y]._mapModeUser.end(); ++it)
     {
@@ -117,7 +118,7 @@ int Join::verifModeChannel(Channel &channel, User *_tabUser, int y)
         }
     }
 
-    std::cout << GREEN << isMode << " && " << isInvitation << std::endl;
+    std::cout << GREEN << isMode << " && " << isInvitation << RESET << std::endl;
     if (isMode == 1 && isInvitation != 1)
     {
         std::cout << "BLOCKKKK JOINNNNN" << std::endl;
