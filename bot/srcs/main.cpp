18797e1846c349cc2b21975632ac283fa52257b8
ascatama@student.42lyon.fr <ascatama@student.42lyon.fr>
2023-11-14 11:30:33 +0100

    bot de merde

--- bot/srcs/main.cpp
+++ bot/srcs/main.cpp
@@ -2,14 +2,15 @@
 
 int main(int ac, char **av)
 {
-    std::cout << "YOOOOOO" << std::endl;
     if (ac != 3)
     {
         std::cout << "Error: Usage /bot...." << std::endl;
         return 1;
     }
 
-    Bot bot = Bot("Bot", std::atoi(av[1]), av[2], "127.0.0.1");
+
+    Bot bot = Bot("Bot", std::atoi(av[1]), av[2], IP);
+
     if (bot.Bot_Start() == 1)
         return 1;
 
@@ -18,5 +19,6 @@ int main(int ac, char **av)
 
     if (bot.Bot_Stop() == 1)
         return 1;
+
     return 0;
 }
